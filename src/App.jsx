5b6ad27b5b676260f4c112ff0b644212f551ade2
Sufiyan <sufiyan7697@gmail.com>
2020-07-23 10:50:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div className="container">
+            <h1>App</h1>
+            <p>Yes</p>
+        </div>
+    )
+}
+
+export default App
